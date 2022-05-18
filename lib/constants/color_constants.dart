65d8886d3b8bd1a20e443d7e6211226157044105
MADHUSRIJ <srimadhu.j@gmail.com>
2022-05-18 10:20:38 +0530

    color

--- lib/constants/color_constants.dart
+++ lib/constants/color_constants.dart
@@ -3,5 +3,10 @@ import 'package:flutter/material.dart';
 const Color colorBlack = Color(0xff000000);
 const Color colorWhite = Color(0xffFFFFFF);
 
-
-
+const Color appBg = Color(0xffF9FAFE);
+const Color filBtn = Color(0xffEEF1F5);
+const Color highlights = Color(0xffFBD721);
+const Color btn = Color(0xffFF6A9C);
+const Color fbtn = Color(0xff13D7BC);
+const Color txt = Color(0xff3E4554);
+const Color heading = Color(0xff645DB1);
